c009b77e3d24b2c90d61325dbc22e3801e677910
Pinku1211 <pinkukumarsarker@gmail.com>
2023-12-08 23:54:28 +0600

    added two files

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Aos from 'aos';
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 
 const Service = ({ card }) => {
     useEffect(() => {
